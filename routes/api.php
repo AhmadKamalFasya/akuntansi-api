5af7184ec58d121fe2d8708755415adab4a9d239
AhmadKamalFasya <fasyaaoi9@gmail.com>
2021-06-20 21:28:29 +0700

    membuat metho index Profile

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,3 +27,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // Route::delete('/transaction/{id}', [TransactionController::class, 'destroy']);
 
 Route::resource('/transaction', TransactionController::class)->except(['create', 'edit']);
+
+Route::get('/profile', [ProfileController::class, 'index']);
